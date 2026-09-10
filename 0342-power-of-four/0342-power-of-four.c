7f015c4a6cad2819b8e7fb49bbc09eaf72e1863a
rangineedikishore060-commits <rangineedikishore060@gmail.com>
2026-09-10 10:08:32 +0530

    Time: 0 ms (100%), Space: 8.4 MB (95.14%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.c
@@ -0,0 +1,13 @@
+bool isPowerOfFour(int n) {
+    if(n<=0) return false;
+    else if(n==1) return true;
+    else{
+        while(n>1){
+            if(n%4==0)
+            n=n/4;
+            else
+            return false;
+        }
+        return true;
+    }
+}
